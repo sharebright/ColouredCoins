df7bff686a8ab5492c51e9ac7efe48665742c608
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2015-06-22 17:45:31 -0700

    Add MVC web app, start modeling

---
+++ WebApplication/Models/PropertyViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    public class PropertyViewModel
+    {
+        public String ID { get; set; }
+
+        public String Name { get; set; }
+
+        public String Description { get; set; }
+    }
+}
