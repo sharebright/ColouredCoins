3d0c2bdbfd26efd725ab275eb55701670725d1d2
Lucas Cullen <lucas@bitcoinbrisbane.com.au>
2015-06-23 20:35:02 -0700

    Add mocks

---
+++ WebApplication/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication.Models
+{
+    public class User
+    {
+        public String Name { get; set; }
+
+        public String AssetAddress { get; set; }
+
+        public String BTCAddress { get; set; }
+
+        public String WIFKey { get; set; }
+    }
+}
--- WebApplication/Repositories/Mock.cs
+++ WebApplication/Repositories/Mock.cs
@@ -5,7 +5,7 @@ using System.Web;
 
 namespace WebApplication.Repositories
 {
-    public class Mock : IPropertyRepository
+    public class Mock : IPropertyRepository, IUserRepository
     {
         public IQueryable<Models.PropertyViewModel> All
         {
@@ -21,6 +21,31 @@ namespace WebApplication.Repositories
                     });
 
                 return mocks.AsQueryable<Models.PropertyViewModel>();
+            }
+        }
+
+        public IQueryable<Models.User> All
+        {
+            get
+            {
+                List<Models.User> mocks = new List<Models.User>(3);
+                mocks.Add(new Models.User()
+                {
+                    Name = "Alice",
+                    AssetAddress = "mgJbos81tWctowDWD1q3EjkZhGmcysj5rr",
+                    BTCAddress = "mgJbos81tWctowDWD1q3EjkZhGmcysj5rr",
+                    WIFKey = "cN2DYQQaxDXCp8EJTP9Y8wFMpjQCc7VKThf7ZuxgCvLMaCpoW2x3"
+                });
+
+                mocks.Add(new Models.User()
+                {
+                    Name = "Bob",
+                    AssetAddress = "mzx9qRvqc3SV7RjmQVJo3MY315oTnwsLjh",
+                    BTCAddress = "mzx9qRvqc3SV7RjmQVJo3MY315oTnwsLjh",
+                    WIFKey = "cS4wZpG8WZVHAo8hKfHUJDzEzZyz1LEP288kBXH45fHE96KhjXB2"
+                });
+
+                return mocks.AsQueryable<Models.User>();
             }
         }
     }
